0dfa9b1be3c3233b2a1440bf77833460bc9964ad
amoriotechnology <madhu@amoriotech.com>
2022-08-11 19:27:11 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Amorio Technology Login Details</title>
+</head>
+<body>
+   <br><br><br><br>
+    <div class="container">
+        <h3 style="margin-bottom: 30px;" class="text-center">Project Management Login Details</h3>
+      <div class="row">
+        <div class="col-lg-4">
+            <div class="card">
+                <img class="card-img-top" src="img/1.png" alt="Card image" style="width:50%">
+                <div class="card-body">
+                  <h4 class="card-title">Slack Login</h4>
+                  <a href="https://app.slack.com/client/T03S2LB727R/D03SH5E71BM" target="_blank" class="btn btn-primary">Slack Login</a>
+                </div>
+              </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <img class="card-img-top" src="img/2.png" alt="Card image" style="width:50%">
+                <div class="card-body">
+                  <h4 class="card-title">Asana Login</h4>
+                  <a href="https://app.asana.com/0/home/1202719863204590" target="_blank" class="btn btn-primary">Asana Login</a>
+                </div>
+              </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <img class="card-img-top" src="img/3.png" alt="Card image" style="width:50%">
+                <div class="card-body">
+                  <h4 class="card-title">Jira Login</h4>
+                  <a href="https://amorio.atlassian.net/jira/software/projects/STOCKEAI/boards/1" target="_blank" class="btn btn-primary">Jira Login</a>
+                </div>
+              </div>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-lg-4">
+            <div class="card">
+                <img class="card-img-top" src="img/1.png" alt="Card image" style="width:50%">
+                <div class="card-body">
+                  <h4 class="card-title">Slack Login</h4>
+                  <a href="https://app.slack.com/client/T03S2LB727R/D03SH5E71BM" target="_blank" class="btn btn-primary">Slack Login</a>
+                </div>
+              </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <img class="card-img-top" src="img/2.png" alt="Card image" style="width:50%">
+                <div class="card-body">
+                  <h4 class="card-title">Asana Login</h4>
+                  <a href="https://app.asana.com/0/home/1202719863204590" target="_blank" class="btn btn-primary">Asana Login</a>
+                </div>
+              </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <img class="card-img-top" src="img/3.png" alt="Card image" style="width:50%">
+                <div class="card-body">
+                  <h4 class="card-title">Jira Login</h4>
+                  <a href="https://amorio.atlassian.net/jira/software/projects/STOCKEAI/boards/1" target="_blank" class="btn btn-primary">Jira Login</a>
+                </div>
+              </div>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
